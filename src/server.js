53169aa7d99cbf6dc1c2c7010bf520ef93166d73
codecraftwt <codecraftwt@gmail.com>
2026-04-01 18:06:38 +0530

    Add CORS middleware to server configuration, allowing requests from specified client origins. This enhances security by controlling access to the API based on environment variables.

--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ const dotenv = require("dotenv");
 const app = require("./app");
 const connectDB = require("./config/db");
 const ensureAdmin = require("./seed/ensureAdmin");
-
+const cors = require("cors");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 const startServer = async () => {
+  app.use(cors(corsOptions));
   await connectDB();
   await ensureAdmin();
   app.listen(PORT, () => {
